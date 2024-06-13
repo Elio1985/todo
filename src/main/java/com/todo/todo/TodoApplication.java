84427e65efe704e4f7adbfd7ce88aeb6fa0b0454
Soleil <andreesaintlouis311@gmail.com>
2024-06-13 17:55:11 -0400

    more stuff

--- src/main/java/com/todo/todo/TodoApplication.java
+++ src/main/java/com/todo/todo/TodoApplication.java
@@ -2,8 +2,10 @@ package com.todo.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.todo.todo")
 public class TodoApplication {
 
 	public static void main(String[] args) {
